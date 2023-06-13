3d65fa19c27012c6dbfd2025bb0eab6cfa9d4ee0
emaevgenieva <47190060+emaevgenieva@users.noreply.github.com>
2023-06-13 22:42:12 +0300

    Create HW_16.h

---
+++ week/HW_16.h
@@ -0,0 +1,15 @@
+#pragma once
+
+void lab_16_01(void);
+void lab_16_03(void);
+void lab_16_04(void);
+void HW_16_01(void);
+void HW_16_02(void);
+void HW_16_03(void);
+void HW_16_04(void);
+void HW_16_05(void);
+void HW_16_06(void);
+void HW_16_07(void);
+void HW_16_08(void);
+void HW_16_09(void);
+void HW_16_10(void);
