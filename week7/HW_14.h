028dcf3e7c7015878a1a1ffaa92e684bf2e0e874
emaevgenieva <47190060+emaevgenieva@users.noreply.github.com>
2023-06-13 22:37:10 +0300

    Create HW_14.h

---
+++ week7/HW_14.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+void hw_14_01(void);
+void hw_14_02(void);
+void hw_14_03(void);
+void hw_14_04(void);
+void hw_14_05(void);
+void hw_14_06(void);
+void hw_14_07(void);
+void hw_14_08(void);
+void hw_14_09(void);
+void hw_14_10(void);
+
+
+
+
