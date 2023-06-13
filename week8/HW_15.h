d631e250cc53f1a8288313e83685b993de84ddde
emaevgenieva <47190060+emaevgenieva@users.noreply.github.com>
2023-06-13 22:40:24 +0300

    Create HW_15.h

---
+++ week8/HW_15.h
@@ -0,0 +1,20 @@
+#pragma once
+
+void read_input(const char* input);
+void print_array(char* arrayInaput);
+void print_input(const char* arr);
+void print_input_counter_NewLine(const char* arr);
+void print_delete_space(char* arr);
+void print_text_letters(const char* arr);
+void print_rows_WithLength_over_3(char* arr);
+float print_average_length(char* arr);
+void print_text_without_space_tab(char* arr);
+void print_reverse_string(char* arr);
+void print_reverse_row_string(char* arr);
+void print_input_counter_Letters_numbers(char* arr);
+void print_row_over_80_symbols(char* arr);
+void print_remove_spacia_tab(char* arr);
+void print_Upper_to_Lower(char* arr);
+void print_revers_row_by_row(char* arr);
+char* Cut_String_to_Whitespace(char* pString);
+char* Cut_String_to_NewLine(char* pString);
