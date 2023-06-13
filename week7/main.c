db96b4f71a1d9537d61a6ee107d1041706dc3285
emaevgenieva <47190060+emaevgenieva@users.noreply.github.com>
2023-06-13 22:38:19 +0300

    Create main.c

---
+++ week7/main.c
@@ -0,0 +1,28 @@
+#include "HW_14.h"
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<ctype.h>
+#include <locale.h>
+
+#define MY_TEST_STR "Hello World!"
+#define STR_SUB 15
+#define STR 100
+
+int main(void) {
+
+
+	//hw_14_01();
+	//hw_14_02();
+	//hw_14_03();
+	//hw_14_04();
+	//hw_14_05();
+	//hw_14_06();
+	//hw_14_07();
+	//hw_14_08();
+	//hw_14_09();
+	//hw_14_10();
+   
+
+}	
